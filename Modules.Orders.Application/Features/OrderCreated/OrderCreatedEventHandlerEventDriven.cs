5278f977b9a4d9b7d6a94fc3f5cf791940d8b519
michaeldsdomingo <michael.ds.domingo@gmail.com>
2024-07-05 02:22:28 -0700

    implemented load balancer

--- Modules.Orders.Application/Features/OrderCreated/OrderCreatedEventHandlerEventDriven.cs
+++ Modules.Orders.Application/Features/OrderCreated/OrderCreatedEventHandlerEventDriven.cs
@@ -1,5 +1,6 @@
 ﻿using MassTransit;
 using MediatR;
+using Microsoft.Extensions.Logging;
 using Modules.Orders.Domain.Entities;
 using Modules.Orders.IntegrationEvents;
 
@@ -8,14 +9,20 @@ namespace Modules.Orders.Application.Features.OrderCreated;
 internal class OrderCreatedEventHandlerEventDriven : INotificationHandler<OrderCreatedDomainEvent>
 {
     private readonly IPublishEndpoint _publishEndpoint;
+    private readonly ILogger<OrderCreatedEventHandlerEventDriven> _logger;
 
-    public OrderCreatedEventHandlerEventDriven(IPublishEndpoint publishEndpoint)
+    public OrderCreatedEventHandlerEventDriven(
+        IPublishEndpoint publishEndpoint, 
+        ILogger<OrderCreatedEventHandlerEventDriven> logger)
     {
         _publishEndpoint = publishEndpoint;
+        _logger = logger;
     }
 
     public async Task Handle(OrderCreatedDomainEvent notification, CancellationToken cancellationToken)
     {
+        _logger.LogInformation($"OrderCreatedDomainEvent started - {notification.OrderId}");
+
         await _publishEndpoint.Publish(new OrderCreatedIntegrationEvent(
             notification.OrderId, 
             notification.ProductId, 
